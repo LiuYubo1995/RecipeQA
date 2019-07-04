9ab811031b87af92cbb1e0349d03d312eaad33cb
LiuYubo1995 <liuyubo0208@hotmail.com>
2019-07-04 13:57:45 +0100

    my own model with multiattention

--- my_own_model/impatient_reader_model_visual.py
+++ my_own_model/impatient_reader_model_visual.py
@@ -14,7 +14,7 @@ class WordLevel(nn.Module):
         self.lstm = nn.LSTM(1024, word_hidden_size, num_layers=1, 
                         bidirectional=True, dropout=0.2)
 
-    def forward(self, input):
+    def forward(self, input):  
         sentences = input
         if torch.cuda.is_available():
             character_ids = batch_to_ids(sentences).cuda()
@@ -25,7 +25,7 @@ class WordLevel(nn.Module):
         output, (hidden,_) = self.lstm(embedded)
         hidden_output = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1)
 
-        return output, hidden_output 
+        return output, hidden_output
 
 
 class SentLevel(nn.Module):
@@ -84,7 +84,7 @@ class Text_Net(nn.Module):
 
         output, hidden_output = self.text_net(output_step)
 
-        return output, hidden_output 
+        return output, hidden_output, output_step 
 
 
 class Question_Net(nn.Module):       
@@ -108,7 +108,7 @@ class Question_Net(nn.Module):
 
         output, hidden_output = self.sen_net(output_word)
 
-        return output, hidden_output
+        return output, hidden_output, output_word
 
 
 class linear_attention(nn.Module):
@@ -121,7 +121,7 @@ class linear_attention(nn.Module):
         H = torch.tanh(self.fc1(question)+self.fc2(vector))
         attention_score = F.softmax(self.fc3(H), dim=0)
         x = torch.matmul(attention_score.permute(1,2,0), question.permute(1,0,2))
-        return x.squeeze(1)
+        return x.squeeze(1), attention_score.permute(1, 2, 0) 
 
 class alternating_co_attention(nn.Module):
     def __init__(self, batch_size, question_dim, vector_dim, attention_dim):
@@ -135,14 +135,14 @@ class alternating_co_attention(nn.Module):
             g = torch.zeros(question.size(1), self.dim).cuda()
         else: 
             g = torch.zeros(question.size(1), self.dim)  
-        temp_vector = self.question_g_attention(question, g)
-        c_vector = self.context_q_attention(context, temp_vector)
-        q_vector = self.question_c_attention(question, c_vector)
-        return q_vector, c_vector 
+        temp_vector, _ = self.question_g_attention(question, g)
+        c_vector, score_q2c = self.context_q_attention(context, temp_vector)
+        q_vector, score_c2q = self.question_c_attention(question, c_vector)
+        return q_vector, c_vector, score_c2q, score_q2c
 
 
 class MultiAttention(nn.Module):
-    def __init__(self, word_hidden_size, sent_hidden_size, batch_size):
+    def __init__(self, word_hidden_size, sent_hidden_size, batch_size, use_lexical):
         super(MultiAttention, self).__init__() 
         self.dim = word_hidden_size*2
         self.batch_size = batch_size
@@ -150,38 +150,110 @@ class MultiAttention(nn.Module):
         self.fc2 = nn.Linear(self.dim, self.dim) # W_a_k
         self.fc3 = nn.Linear(self.dim, self.dim) # W_q 
         self.attention = alternating_co_attention(batch_size, self.dim, self.dim, self.dim) 
-        self.attention1 = linear_attention(self.dim, self.dim, self.dim)
-        self.linear = nn.Linear(self.dim*2, self.dim)
-    def forward(self, answer_hidden, question_output, context_output, num_attention=2):
-        '''
-        input
-        answer_hidden (batch_size, dim)
-        question_output (4, batch_size, dim)
-        '''
+        self.lexical_question = linear_attention(self.dim, self.dim, self.dim)
+        self.lexical_context = linear_attention(self.dim, self.dim, self.dim)
+        self.linear = nn.Linear(self.dim*2, self.dim) 
+        self.use_lexical = use_lexical
+        if self.use_lexical:
+            self.lexical_projection_q = nn.Linear(self.dim, self.dim) 
+            self.lexical_projection_c= nn.Linear(self.dim, self.dim)
+    def forward(self, answer_hidden, question_output, context_output, context_input, question_input, num_attention=2):
+
         if torch.cuda.is_available():  
             u = torch.zeros(question_output.size(1), self.dim, requires_grad = True).cuda()
         else:
             u = torch.zeros(question_output.size(1), self.dim, requires_grad = True)
-            
+
         for i in range(num_attention):
             q_new = torch.tanh(self.fc1(u) + self.fc2(answer_hidden) + self.fc3(question_output))
-            q_attention_vector, context_attention_vector = self.attention(question_output, context_output)
+            q_attention_vector, context_attention_vector, score_c2q, score_q2c = self.attention(question_output, context_output)
+            
+
+            if self.use_lexical: 
+                q_lexical = torch.tanh(torch.matmul(score_c2q, question_input.permute(1,0,2))).squeeze(1)
+                c_lexical = torch.tanh(torch.matmul(score_q2c, context_input.permute(1,0,2))).squeeze(1)
+                q_attention_vector = torch.tanh(self.lexical_projection_q(q_lexical)) + q_attention_vector
+                context_attention_vector = torch.tanh(self.lexical_projection_c(c_lexical)) + context_attention_vector
+
             u = torch.tanh(self.linear(torch.cat((q_attention_vector, context_attention_vector), dim=1))) 
         return u 
 
+class MultiAttention_image(nn.Module):
+    def __init__(self, word_hidden_size, sent_hidden_size, batch_size, use_lexical):
+        super(MultiAttention_image, self).__init__() 
+        self.dim = word_hidden_size*2
+        self.batch_size = batch_size
+        self.fc1 = nn.Linear(self.dim, self.dim) # W_u 
+        self.fc2 = nn.Linear(self.dim, self.dim) # W_a_k
+        self.fc3 = nn.Linear(self.dim, self.dim) # W_q 
+
+        self.attention_q_c = alternating_co_attention(batch_size, self.dim, self.dim, self.dim) 
+        self.attention_q_v = alternating_co_attention(batch_size, self.dim, self.dim, self.dim) 
+        self.attention_c_v = alternating_co_attention(batch_size, self.dim, self.dim, self.dim) 
+
+        self.lexical_question = linear_attention(self.dim, self.dim, self.dim)
+        self.lexical_context = linear_attention(self.dim, self.dim, self.dim)
+        self.linear = nn.Linear(self.dim*6, self.dim) 
+        self.use_lexical = use_lexical 
+
+        if self.use_lexical:
+            self.lexical_projection_q_c = nn.Linear(self.dim, self.dim) 
+            self.lexical_projection_c_q = nn.Linear(self.dim, self.dim)
+            self.lexical_projection_q_v = nn.Linear(self.dim, self.dim)
+            self.lexical_projection_v_q = nn.Linear(1000, self.dim)
+            self.lexical_projection_c_v = nn.Linear(self.dim, self.dim)
+            self.lexical_projection_v_c = nn.Linear(1000, self.dim)
+
+
+    def forward(self, answer_hidden, question_output, context_output, image_output, context_input, question_input, image_input, num_attention=2):
+
+        if torch.cuda.is_available():  
+            u = torch.zeros(question_output.size(1), self.dim, requires_grad = True).cuda()
+        else:
+            u = torch.zeros(question_output.size(1), self.dim, requires_grad = True)
+
+        for i in range(num_attention):
+            q_new = torch.tanh(self.fc1(u) + self.fc2(answer_hidden) + self.fc3(question_output))
+            q_attention_vector_c, c_attention_vector_q, score_c2q, score_q2c = self.attention_q_c(question_output, context_output)
+            q_attention_vector_v, v_attention_vector_q, score_v2q, score_q2v = self.attention_q_v(question_output, image_output)
+            c_attention_vector_v, v_attention_vector_c, score_v2c, score_c2v = self.attention_q_v(context_output, image_output)
+
+            if self.use_lexical: 
+                q_lexical_c = torch.tanh(torch.matmul(score_c2q, question_input.permute(1,0,2))).squeeze(1)
+                c_lexical_q = torch.tanh(torch.matmul(score_q2c, context_input.permute(1,0,2))).squeeze(1)
+                q_attention_vector_c = torch.tanh(self.lexical_projection_q_c(q_lexical_c)) + q_attention_vector_c
+                c_attention_vector_q = torch.tanh(self.lexical_projection_c_q(c_lexical_q)) + c_attention_vector_q
+
+                q_lexical_v = torch.tanh(torch.matmul(score_v2q, question_input.permute(1,0,2))).squeeze(1)
+                v_lexical_q = torch.tanh(torch.matmul(score_q2v, image_input.permute(1,0,2))).squeeze(1)
+                q_attention_vector_v = torch.tanh(self.lexical_projection_q_v(q_lexical_c)) + q_attention_vector_v
+                v_attention_vector_q = torch.tanh(self.lexical_projection_v_q(v_lexical_q)) + v_attention_vector_q
+
+                v_lexical_c = torch.tanh(torch.matmul(score_c2v, image_input.permute(1,0,2))).squeeze(1)
+                c_lexical_v = torch.tanh(torch.matmul(score_v2c, context_input.permute(1,0,2))).squeeze(1)
+                v_attention_vector_c = torch.tanh(self.lexical_projection_v_c(v_lexical_c)) + v_attention_vector_c
+                c_attention_vector_v = torch.tanh(self.lexical_projection_c_v(c_lexical_v)) + c_attention_vector_v
+
+            u = torch.tanh(self.linear(torch.cat((q_attention_vector_c, c_attention_vector_q, q_attention_vector_v, v_attention_vector_q, c_attention_vector_v, v_attention_vector_c), dim=1))) 
+        return u
+
 
 class Impatient_Reader_Model(nn.Module):       
-    def __init__(self, word_hidden_size, sent_hidden_size, batch_size, num_attention):
+    def __init__(self, word_hidden_size, sent_hidden_size, batch_size, use_lexical, use_image, num_attention):
         super(Impatient_Reader_Model, self).__init__()
         self.text = Text_Net(word_hidden_size, sent_hidden_size)
         self.question = Question_Net(word_hidden_size, sent_hidden_size) 
-        self.attention = MultiAttention(word_hidden_size, sent_hidden_size, batch_size)
+        self.attention = MultiAttention(word_hidden_size, sent_hidden_size, batch_size, use_lexical)
         self.dim  = 2*word_hidden_size
         self.choice = ChoiceNet(word_hidden_size)
-        #self.images = nn.LSTM(1000, word_hidden_size, bidirectional=True) 
+        self.use_image  = use_image
         self.num_attention = num_attention
         self.q = nn.Linear(self.dim, self.dim)
         self.a = nn.Linear(self.dim, self.dim)
+        if self.use_image: 
+            self.images = nn.LSTM(1000, word_hidden_size, bidirectional=True) 
+            self.attention_image = MultiAttention_image(word_hidden_size, sent_hidden_size, batch_size, use_lexical)
+    
     def Infersent(self, x1, x2):
         return torch.cat((x1, x2, torch.abs(x1 - x2), x1 * x2), 1)
 
@@ -190,22 +262,27 @@ class Impatient_Reader_Model(nn.Module):
         input_question = transport_1_0_2(input_question)
         input_choice = transport_1_0_2(input_choice) 
         input_images = transport_1_0_2_image(input_images) 
+        if self.use_image:
+            input_images = extract_image_features(input_images, image_path)
+            image_output, _  = self.images(input_images)
 
-        #input_images = extract_image_features(input_images, image_path)
-
-        context_output, _ = self.text(input_context)
-        question_output, question_final_hidden = self.question(input_question)
-        #image_output, _  = self.images(input_images) 
-        output_choice_list = [] 
-
+        context_output, _, context_input = self.text(input_context)
+        question_output, question_final_hidden, question_input = self.question(input_question)
+         
+        output_choice_list = []
         for i in input_choice:   
             output_choice, hidden_output_choice = self.choice(i)
-            u = self.attention(hidden_output_choice, question_output, context_output, self.num_attention)
+            if self.use_image:
+                u = self.attention_image(hidden_output_choice, question_output, context_output, image_output, context_input, question_input, input_images, self.num_attention)
+            else:
+                u = self.attention(hidden_output_choice, question_output, context_output, context_input, question_input, self.num_attention)
+
             q_u = torch.tanh(self.q(question_final_hidden)) + u
             similarity_scores = torch.sum(torch.mul(q_u, torch.tanh(self.a(hidden_output_choice))), dim=1)
             # similarity_scores = self.Infersent(g, hidden_output_choice)
             # similarity_scores = self.dropout(torch.tanh(self.fc3(similarity_scores)))
             # similarity_scores = self.fc4(similarity_scores)
+
             output_choice_list.append(similarity_scores) 
             
         return output_choice_list 
--- my_own_model/train.py
+++ my_own_model/train.py
@@ -24,6 +24,8 @@ def get_args():
     parser.add_argument("--saved_path", type=str, default="trained_models")
     parser.add_argument("--load_model", type=str, default=None)
     parser.add_argument("--num_attention", type=str, default=2)
+    parser.add_argument("--use_lexical", type=str, default=True) 
+    parser.add_argument("--use_image", type=str, default=True)  
     args = parser.parse_args() 
     return args 
 
@@ -63,8 +65,6 @@ def eval_run_batch(model, val_context, val_question, val_choice, val_answer, val
     return epoch_loss / len(val_context), epoch_acc / len(val_context)
 
 
-
-
 def main(args):
 
     batch_size = args.batch_size
@@ -73,11 +73,13 @@ def main(args):
     word_hidden_size = args.word_hidden_size
     sent_hidden_size = args.sent_hidden_size
     num_attention = args.num_attention
+    use_lexical = args.use_lexical
+    use_image = args.use_image
 
     recipe_context, recipe_images, recipe_question, recipe_choice, recipe_answer = load_cleaned_data('train_cleaned.json')
     recipe_context_val, recipe_images_val, recipe_question_val, recipe_choice_val, recipe_answer_val = load_cleaned_data('val_cleaned.json')
 
-    model = Impatient_Reader_Model(word_hidden_size, sent_hidden_size, batch_size, num_attention) 
+    model = Impatient_Reader_Model(word_hidden_size, sent_hidden_size, batch_size, use_lexical, use_image, num_attention) 
     if args.load_model: 
         model.load_state_dict(torch.load(args.load_model))
         model.eval()
@@ -98,16 +100,16 @@ def main(args):
         train_context_new,train_question_new,train_choice_new,train_answer_new, train_images_new = shuffle_data(recipe_context,recipe_question,recipe_choice,recipe_answer, recipe_images)
         val_context_new,val_question_new,val_choice_new,val_answer_new, val_images_new = shuffle_data(recipe_context_val,recipe_question_val,recipe_choice_val,recipe_answer_val, recipe_images_val)
         
-        # train_context_new = train_context_new[0:5]
-        # train_question_new = train_question_new[0:5]
-        # train_choice_new = train_choice_new[0:5]
-        # train_answer_new = train_answer_new[0:5]
-        # train_images_new = train_images_new[0:5]
-        # val_context_new = val_context_new[0:5]
-        # val_question_new = val_question_new[0:5]
-        # val_choice_new = val_choice_new[0:5]
-        # val_answer_new = val_answer_new[0:5]
-        # val_images_new = val_images_new[0:5]
+        train_context_new = train_context_new[0:5]
+        train_question_new = train_question_new[0:5]
+        train_choice_new = train_choice_new[0:5]
+        train_answer_new = train_answer_new[0:5]
+        train_images_new = train_images_new[0:5]
+        val_context_new = val_context_new[0:5]
+        val_question_new = val_question_new[0:5]
+        val_choice_new = val_choice_new[0:5]
+        val_answer_new = val_answer_new[0:5]
+        val_images_new = val_images_new[0:5] 
         train_context, train_question, train_choice, train_answer, train_images = split_batch(batch_size, train_context_new,train_question_new,train_choice_new,train_answer_new, train_images_new)
         val_context, val_question, val_choice, val_answer, val_images = split_batch(batch_size, val_context_new,val_question_new,val_choice_new,val_answer_new, val_images_new)
 
@@ -122,7 +124,6 @@ def main(args):
 
         train_loss, train_acc = train_run(model, train_context, train_question, train_choice, train_answer, train_images, train_images_feature, optimizer, criterion, batch_size)
         valid_loss, valid_acc = eval_run_batch(model, val_context, val_question, val_choice, val_answer, val_images, val_images_feature, criterion, batch_size)
-        #valid_loss, valid_acc = eval_run(model, recipe_context_valid, recipe_question_valid, recipe_choice_valid, recipe_answer_valid, criterion)
         log_data(args.log_path, train_loss, train_acc, valid_loss, valid_acc)
 
         print(f'| Epoch: {epoch+1:02} | Train Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}% | Val. Loss: {valid_loss:.3f} | Val. Acc: {valid_acc*100:.2f}%')
